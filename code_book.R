149fba6c78be58f918cdff7f0eb975655cf70a6c
David Parra <davidc.parrar@gmail.com>
2017-04-20 21:11:18 -0500

    Final code_book.R

--- code_book.R
+++ code_book.R
@@ -8,19 +8,20 @@ code_book <- function(x, name = "CodeBook.md"){
   source <- "The variables for this data set come from the the coursera link [Dataset](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip), for more information on the raw dataset please refer to [UCI machine learning repository](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones), or the authors of the study:"
   authors <- "> Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012"
   usage <- "to obtain the data set run the following command:\n\n  source(\"run_analysis.R\")\n\n The data set will be store in the data set tidy_df\n"
-  format <- paste("A data frame with ",vectnRows," observations on ",length(vectnames), " variables    ")
+  
   
   #Determine values of table
   vectnames <- names(x)
   vectnRows <- nrow(x)
   vectseq <- 1:length(vectnames)
   type <- unname(sapply(x,class))
+  format <- paste("A data frame with ",vectnRows," observations on ",length(vectnames), " variables    ")
   
   #Open file and write info
   sink(name)
     cat("tidy_df dataset {datasets}")
     cat("\n")
-    cat("# Mean Value of the Human Activity Recognition Using Smartphones Dataset")
+    cat("# Mean Values of the Human Activity Recognition Using Smartphones Dataset")
     cat("\n")
     cat("## Description")
     cat("\n")
